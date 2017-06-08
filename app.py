93cbe04c298262822afa48164bf6769970f87f6d
Pro100-Adm <kirow.alesha@yandex.ru>
2017-06-08 18:14:47 +0300

    Create app.py

--- app.py
+++ app.py
@@ -35,9 +35,9 @@ def wsgi_app(environ, start_response):
         try:
             for i in range(0,len(x)-1):
                 x[i]=float(x[i])
-            y1=(x[1]/x[3])
-            y2=(x[2]/x[1]*x[3])-(x[6]/100*x[3])
-            response_body="Период возврата средств: "+str(y1)+"<br>"+"Ваш месячный доход: "+str(y2)
+            #y1=(x[1]/x[3])
+            #y2=(x[2]/x[1]*x[3])-(x[6]/100*x[3])
+            response_body=x
             start_response(status, response_headers)
             yield response_body.encode()
         except:
